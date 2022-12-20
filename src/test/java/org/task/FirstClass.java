715ed86d52cfb75ba9dc9bb4267d7770624aa556
MAXIMUS1543 <yuvarajray7000@gmail.com>
2022-12-20 20:21:35 +0530

    project structure

---
+++ src/test/java/org/task/FirstClass.java
@@ -0,0 +1,31 @@
+package org.task;
+
+import org.junit.*;
+
+public class FirstClass {
+
+	@BeforeClass
+	public static void bc() {
+		System.out.println("bc");
+	}
+
+	@Before
+	public void b() {
+		System.out.println("b");
+	}
+
+	@Test
+	public void t() {
+		System.out.println("t");
+	}
+
+	@After
+	public void a() {
+		System.out.println("a");
+	}
+
+	@AfterClass
+	public static void ac() {
+		System.out.println("ac");
+	}
+}
